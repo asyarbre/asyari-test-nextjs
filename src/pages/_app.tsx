30cf0b32a945a49d66f0fe650576ec1f121d2a1d
Asya'ri <asyari818@gmail.com>
2023-12-28 18:49:04 +0700

    feat: add dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,14 @@ import type { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 
+import { ThemeProvider } from '@/components/provider/theme-provider';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeProvider attribute='class' defaultTheme='dark'>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
