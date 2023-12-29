62f55ab9b8b5d2836a9f25bd09e6bf8daefedb2a
Asya'ri <asyari818@gmail.com>
2023-12-29 13:29:15 +0700

    feat(type): add login and register

---
+++ src/services/types/index.ts
@@ -0,0 +1,11 @@
+export interface LoginTypes {
+  email: string;
+  password: string;
+}
+
+export interface RegisterTypes {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
