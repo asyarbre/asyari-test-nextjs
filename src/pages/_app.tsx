73b7794bd26a6d48327a9368cad9aa369581440e
Asya'ri <asyari818@gmail.com>
2023-12-29 13:28:38 +0700

    feat: import toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 
 import '@/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { ThemeProvider } from '@/components/provider/theme-provider';
 
@@ -10,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider attribute='class' defaultTheme='dark'>
         <Component {...pageProps} />
       </ThemeProvider>
+      <ToastContainer />
     </>
   );
 }
