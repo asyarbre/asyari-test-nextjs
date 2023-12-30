425264f89d7f750e8cee79532a4beaf4863d884d
Asya'ri <asyari818@gmail.com>
2023-12-30 07:25:55 +0700

    refactor: detail book

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -4,15 +4,7 @@ import { useRouter } from 'next/router';
 import * as React from 'react';
 
 import { Button } from '@/components/ui/button';
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 
 import { handleDetailBook } from '@/services/books';
 import { DetailBookTypes } from '@/services/types';
@@ -41,47 +33,72 @@ export default function BookDetailPage() {
           <span>Back to Home</span>
         </Link>
       </Button>
-      <Table>
-        <TableCaption>Detail Book</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className='w-[100px]'>ID</TableHead>
-            <TableHead>User ID</TableHead>
-            <TableHead>ISBN</TableHead>
-            <TableHead>Title</TableHead>
-            <TableHead>Subtitle</TableHead>
-            <TableHead>Author</TableHead>
-            <TableHead>Published</TableHead>
-            <TableHead>Publisher</TableHead>
-            <TableHead>Pages</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Website</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {book ? (
-            <TableRow key={book.id}>
-              <TableCell className='font-medium'>{book.id}</TableCell>
-              <TableCell>{book.user_id}</TableCell>
-              <TableCell>{book.isbn}</TableCell>
-              <TableCell>{book.title}</TableCell>
-              <TableCell>{book.subtitle}</TableCell>
-              <TableCell>{book.author}</TableCell>
-              <TableCell>{book.published}</TableCell>
-              <TableCell>{book.publisher}</TableCell>
-              <TableCell>{book.pages}</TableCell>
-              <TableCell>{book.description}</TableCell>
-              <TableCell>{book.website}</TableCell>
-            </TableRow>
-          ) : (
+      <div className='mb-10'>
+        <Table>
+          <h1 className='text-xl font-bold mb-5'>Book Detail</h1>
+          <TableBody>
             <TableRow>
-              <TableCell colSpan={10} className='text-center'>
-                No data
+              <TableCell className='font-medium'>ID</TableCell>
+              <TableCell>{book?.id}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>User ID</TableCell>
+              <TableCell>{book?.user_id}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>ISBN</TableCell>
+              <TableCell>{book?.isbn}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>Title</TableCell>
+              <TableCell>{book?.title}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>Subtitle</TableCell>
+              <TableCell>{book?.subtitle}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>Author</TableCell>
+              <TableCell>{book?.author}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>Published</TableCell>
+              <TableCell>
+                {book?.published
+                  ? new Date(book?.published).toLocaleDateString()
+                  : '-'}
               </TableCell>
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            <TableRow>
+              <TableCell className='font-medium'>Publisher</TableCell>
+              <TableCell>{book?.publisher}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>Pages</TableCell>
+              <TableCell>{book?.pages}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>Description</TableCell>
+              <TableCell>{book?.description}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className='font-medium'>Website</TableCell>
+              <TableCell>
+                <Link
+                  href={
+                    book?.website ? book?.website : 'https://www.gramedia.com'
+                  }
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='text-blue-500 hover:underline'
+                >
+                  Go To Website
+                </Link>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </main>
   );
 }
